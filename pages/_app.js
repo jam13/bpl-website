a7203a44201a0acc93519dd1f009988e8a4a59fc
jam13 <jamie.neil@gmail.com>
2021-10-05 19:35:11 +0100

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import "@/styles/globals.css";
+import { useRouter} from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // https://github.com/vercel/next.js/issues/9992
+  const { asPath } = useRouter();
+  return <Component {...pageProps} key={asPath} />;
 }
 
 export default MyApp;
